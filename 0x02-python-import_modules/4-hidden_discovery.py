5b82e393d2a330f85154e50663a273c31bcc524c
ch-canaza <ch-canaza@hotmail.com>
2020-05-01 03:42:47 +0000

    hidden modules

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4 as str_modules
+    modules = dir(str_modules)
+    for i in range(0, len(modules)):
+        if (modules[i][0] != '_'):
+                  print(modules[i])
