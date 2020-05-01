819a6cca436767dbe0cec510810abeeceba8cd4a
ch-canaza <ch-canaza@hotmail.com>
2020-05-01 03:04:43 +0000

    declare var above

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    if (len(sys.argv) < 2):
-        print("{:d}".format(0))
-    else:
-        for i in range(1, len(sys.argv)):
-            result = 0
-            result = int(sys.argv[i]) + result
-            print("{:d}".format(result))
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{:d}".format(result))
