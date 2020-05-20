2f2a335e2396fbf1ed175388ae8f46838c9ee40f
ch-canaza <ch-canaza@hotmail.com>
2020-05-19 21:37:52 -0500

    fix pep8

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,24 +7,24 @@ class Square:
     Attributes:
         __size (int): size of a side of the square
     """
-
     def __init__(self, size=0):
         """initializes the square
         Args:
-        size (int): size of a side of the square
-        Returns:None
+            size (int): size of a side of the square
+        Returns:
+            None
         """
-    if type(size) != int:
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    else:
-        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
 
     def area(self):
         """calculates the square's area
         Returns:
             The area of the square
         """
-        size = self.__size
-        return(size * size)
+        return (self.__size) ** 2
