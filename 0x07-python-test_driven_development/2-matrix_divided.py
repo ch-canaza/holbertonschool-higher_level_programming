26ce89878079471cc9219979fcd75aa718b1a78b
ch-canaza <ch-canaza@hotmail.com>
2020-05-22 12:25:16 -0400

    first commit

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+function that divides all elements of a matrix.
+"""
+
+
+def matrix_divided(matrix, div):
+
+    """ Args:
+        matrix (:obj:`list` of :obj:`list` of :obj:`int` or :obj:`float`):
+            list of lists of integers or floats
+        div (int or float): divisor
+    Returns:
+        a new matrix containing the quotients
+    Raises:
+        TypeError: if matrix elements are neither int nor float,
+            if matrix rows are not the same size
+        ZeroDivisionError: if dividing by zero
+    """
+    new_matrix = []
+
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for row in range(len(matrix)):
+        for element in range(len(matrix[row])):
+            if type(matrix[row]) is not list and type(matrix[row][element] is
+                            not int or type(matrix[row][element]) is not float):
+                raise TypeError("matrix must be a matrix (list of lists)"
+               " of integers/floats")
+            if len(matrix[0]) != len(matrix[row]):
+                raise TypeError("Each row of the matrix must have the"
+               " same size")
+    new_matrix = [[round(element / div, 2) for element in row]
+                  for row in matrix]
+    return new_matrix
