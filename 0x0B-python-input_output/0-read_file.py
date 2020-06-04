cbc48403c7452a2f8886bd71cfb54685c43c92c2
ch-canaza <ch-canaza@hotmail.com>
2020-06-03 20:45:33 -0500

    fixing utf-8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,8 +3,8 @@
 
 
 def read_file(filename=""):
-    """ function that reads a text and prints in 
+    """ function that reads a text and prints in
         stdout """
-    with open("my_file_0.txt", encoding="UTF8") as f:
+    with open("my_file_0.txt", encoding="utf-8") as f:
         for line in f:
             print(line, end="")
