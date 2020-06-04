5abc5b31a3e3912154456ffc13277553be09996f
ch-canaza <ch-canaza@hotmail.com>
2020-06-03 20:55:49 -0500

    fixing filename

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """ writes a string to a text file and return number of characters """
 
-    with open("filename", 'r+', encoding='utf-8') as f:
+    with open(filename, 'r+', encoding='utf-8') as f:
         return f.write(text)
