528f51f415ab392b3e7c8e091dc3bce65edd0bd3
ch-canaza <ch-canaza@hotmail.com>
2020-07-14 16:45:30 -0500

    fixing some spelling fails

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,8 +7,8 @@
 -- name VARCHAR(256) can’t be null
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table cities already exists, your script should not fail
-CREATE DATABASE IF NOT EXISTS hbn_0d_usa;
-USE hbn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities ( id INT NOT NULL AUTO_INCREMENT,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
