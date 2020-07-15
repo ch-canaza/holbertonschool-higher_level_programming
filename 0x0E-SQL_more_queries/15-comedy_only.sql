84a4ad7d1c98a90333437dbb10f2bfde8b269e05
ch-canaza <ch-canaza@hotmail.com>
2020-07-15 16:55:00 -0500

    fixing spelling

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -5,7 +5,7 @@
 -- Results must be sorted in ascending order by the show title
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_shows.title FROM tv_shows
+SELECT title FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id;
-WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title;
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy" ORDER BY title;
