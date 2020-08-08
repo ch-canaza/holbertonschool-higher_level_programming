57d8151e6184a095ce17488bd2eb8c5161e385a3
ch-canaza <ch-canaza@hotmail.com>
2020-08-08 11:00:32 -0500

    adding print with no format

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * from states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT * from states WHERE name LIKE 'N%' ORDER BY id ASC")
     states = cur.fetchall()
     for state in states:
-        print("{}".format(state))
+        print(state)
     cur.close()
     db.close()
