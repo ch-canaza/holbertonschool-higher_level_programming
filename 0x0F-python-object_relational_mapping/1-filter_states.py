fc19b66f9eb3dffc8668e9d16dbdeaea1a4bdae9
ch-canaza <ch-canaza@hotmail.com>
2020-08-08 10:58:07 -0500

    adding asc

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * from states WHERE name LIKE 'N%' ORDER BY states.id")
+    cur.execute("SELECT * from states WHERE name LIKE 'N%' ORDER BY states.id ASC")
     states = cur.fetchall()
     for state in states:
         print("{}".format(state))
