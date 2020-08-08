9de16ed679c652ae55a1fce87cb2fba9cb8ac50a
ch-canaza <ch-canaza@hotmail.com>
2020-08-08 14:44:00 -0500

    changing line possition

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,12 +2,16 @@
 ''' write a script 14-model_city_fetch_by_state.py that\
     prints all City objects from the database hbtn_0e_14_usa: '''
 
-if __name__ == "__main__":
+
+
     from sys import argv
     from model_state import Base, State
     from model_city import City
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
+
+    if __name__ == "__main__":
+    
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
