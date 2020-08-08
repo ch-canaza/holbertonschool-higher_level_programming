db7b593f3cd4460153c5aa908f8d4da6158a3db4
ch-canaza <ch-canaza@hotmail.com>
2020-08-08 11:18:31 -0500

    pep8 style

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
                            .format(argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-    
-    for state in session.query(State).filter(\
+
+    for state in session.query(State).filter(
             State.name.contains('a')).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
 
