4eaa3ad4c4d44b3457334e745930f42e485ea119
ch-canaza <ch-canaza@hotmail.com>
2020-08-08 14:49:31 -0500

    fix pep8

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-''' write a script 14-model_city_fetch_by_state.py that prints all City objects\
-     from the database hbtn_0e_14_usa: '''
+''' write a script 14-model_city_fetch_by_state.py that prints \
+    all City objects from the database hbtn_0e_14_usa: '''
+
 
 if __name__ == "__main__":
-    
+
     from sys import argv
     from model_state import Base, State
     from model_city import City
