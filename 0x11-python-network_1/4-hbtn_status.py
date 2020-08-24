4b8ae78a9d570c2f09413bd7ddbbf693cea03bde
ch-canaza <ch-canaza@hotmail.com>
2020-08-24 15:44:02 -0500

    pep8 complain

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,7 +2,8 @@
 """ script that fetches https://intranet.hbtn.io/status
 * You must use the package requests
 * You are not allow to import packages other than requests
-* The body of the response must be display like the following example (tabulation before -)
+* The body of the response must be display like the following example
+ (tabulation before -)
 """
 import requests
 
