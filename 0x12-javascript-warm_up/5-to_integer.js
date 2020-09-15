bca7ff064e9387459792eb3c59c8293897e2ec07
ch-canaza <ch-canaza@hotmail.com>
2020-09-15 14:35:25 -0500

    typo correction

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const argument = process.argv[2];
 if (isNaN(argument)) {
   console.log('Not a number');
 } else {
-  console.log('my number: ' + argument);
+  console.log('My number: ' + argument);
 }
