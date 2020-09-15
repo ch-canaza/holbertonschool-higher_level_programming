6e81501d4664455e485d7b35956c54e82315b69b
ch-canaza <ch-canaza@hotmail.com>
2020-09-15 15:24:56 -0500

    printing a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argument = process.argv[2];
+if (isNaN(argument)) {
+  console.log('Missing size');
+} else {
+  for (let x = 1; x <= argument; x++) {
+    let row = '';
+    for (let y = 0; y < argument; y++) row += '#';
+    console.log(row);
+  }
+}
